2963269a8c14fb1977528320a3e23d9b73be440b
Jesus <Jesus@INDIGO>
2023-05-23 01:37:24 +0300

    Add project files.

---
+++ QuickSort/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static readonly Random random = new Random();
+
+        public static void DisplayArray(IEnumerable items)
+        {
+            foreach (var value in items)
+            {
+                Console.Write($"[{value}] ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main()
+        {
+            const int Count = 16;
+
+            int[] nums = new int[Count];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = random.Next(-300, 300);
+            }
+
+            Console.WriteLine("Array before sorting:");
+            DisplayArray(nums);
+
+            Console.WriteLine("Sorting by ascending:");
+            QuickSort.Sort(nums);
+            DisplayArray(nums);
+
+            Console.WriteLine("Sorting by Descending:");
+            QuickSort.Sort(nums, QuickSort.SortType.Descending);
+            DisplayArray(nums);
+
+            char[] chrs = new char[Count];
+            for (int j = 0; j < chrs.Length; j++)
+            {
+                chrs[j] = (char)('a' + random.Next(26));
+            }
+
+            Console.WriteLine("Array before sorting:");
+            DisplayArray(chrs);
+
+            Console.WriteLine("Sorting by ascending:");
+            QuickSort.Sort(chrs, QuickSort.SortType.Ascending);
+            DisplayArray(chrs);
+
+            Console.WriteLine("Sorting by Descending:");
+            QuickSort.Sort(chrs, QuickSort.SortType.Descending);
+            DisplayArray(chrs);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QuickSort/QuickSort.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace QuickSort
+{
+    static class QuickSort
+    {
+        public enum SortType
+        {
+            Ascending,
+            Descending
+        }
+
+        private static void Swap<T>(ref T a, ref T b)
+        {
+            T c = a;
+            a = b;
+            b = c;
+        }
+
+        private static int Compare<T>(T a, T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b);
+        }
+
+        private static void SortArray<T>(T[] array, int first, int last, int direction) where T : IComparable<T>
+        {
+            int start = first;
+            int end = last;
+            int middle = start + ((end - start) / 2);
+            T pivot = array[middle];
+
+            while (start <= end)
+            {
+                while (direction * Compare(array[start], pivot) < 0)
+                {
+                    start++;
+                }
+
+                while (direction * Compare(array[end], pivot) > 0)
+                {
+                    end--;
+                }
+
+                if (start <= end)
+                {
+                    Swap(ref array[start], ref array[end]);
+                    start++;
+                    end--;
+                }
+            }
+
+            if (first < end)
+            {
+                SortArray(array, first, end, direction);
+            }
+
+            if (start < last)
+            {
+                SortArray(array, start, last, direction);
+            }
+        }
+
+        public static void Sort<T>(T[] array) where T : IComparable<T>
+        {
+            Sort(array, SortType.Ascending);
+        }
+
+        public static void Sort<T>(T[] array, SortType sorting) where T : IComparable<T>
+        {
+            if (array != null && array.Length > 1)
+            {
+                int first = 0;
+                int last = array.Length - 1;
+                int direction = (sorting == SortType.Ascending) ? 1 : -1;
+                SortArray(array, first, last, direction);
+            }
+        }
+    }
+}
